ed832d8a6803051ec46653144a7dbcc601ed8a86
Jacqueline Navarro <154712329+jacquelinenavarro@users.noreply.github.com>
2024-07-17 00:36:40 +0000

    lojaroupa15072024

--- LojaRoupas/LojaRoupasTeste.java
+++ LojaRoupas/LojaRoupasTeste.java
@@ -4,14 +4,15 @@ public class LojaRoupasTeste {
     public static void main(String[] args) {
         Produto camiseta = new Produto("Camiseta", 50.0);
         Produto calca = new Produto("Calça", 80.0);
-        Cliente cliente = new Cliente("Maria");
-        Funcionario funcionario = new Funcionario("João");
+        Cliente cliente = new Cliente("Jacqueline Navarro");
+        Funcionario funcionario = new Funcionario("João Silva");
 
         double valorVenda = camiseta.getPreco() + calca.getPreco();
         double valorComDescontoCliente = cliente.aplicarDesconto(valorVenda);
         double valorComDescontoFuncionario = funcionario.aplicarDesconto(valorVenda);
 
         System.out.println("Cliente: " + cliente.getNome());
+        System.out.println("Funcionario: " + funcionario.getNome());
         System.out.println("Cashback acumulado: $" + cliente.getCashback());
         System.out.println("Valor da venda com desconto de cliente: $" + valorComDescontoCliente);
         System.out.println("Valor da venda com desconto de funcionário: $" + valorComDescontoFuncionario);

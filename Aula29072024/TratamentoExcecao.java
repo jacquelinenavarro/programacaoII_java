b47c92f6b1530ea38a4693aff7350c425b3d97c2
Jacqueline Navarro <154712329+jacquelinenavarro@users.noreply.github.com>
2024-07-29 22:53:41 +0000

    aula2907

---
+++ Aula29072024/TratamentoExcecao.java
@@ -0,0 +1,12 @@
+// https://pythonhelp.wordpress.com/2012/09/25/filas-e-pilhas-em-python/
+
+// int [] x = new int [3];
+
+// x[8] = 
+
+// Arreys index Out Bold Exception 
+// x = 3;
+// y = 0;
+// z = x/y
+
+// Troweble - Ancestral

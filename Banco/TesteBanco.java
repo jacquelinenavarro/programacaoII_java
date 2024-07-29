d8fdce6cf29f21167b603b87c754c006fd7990c8
Jacqueline Navarro <154712329+jacquelinenavarro@users.noreply.github.com>
2024-07-29 02:37:47 +0000

    print

--- Banco/TesteBanco.java
+++ Banco/TesteBanco.java
@@ -3,11 +3,11 @@ package Banco;
 public class TesteBanco {
     public static void main(String[] args) {
         Conta conta = new Conta(1001, "Jacqueline Navarro", 100.0);
-        ContaNegocio contaNegocio = new ContaNegocio(1002, "Jose Silva", 200.0, 500.0);
+        ContaNegocio contaNegocio = new ContaNegocio(1002, "Jose Silva", 150.0, 500.0);
 
         // UPCasting
         Conta conta1 = contaNegocio;
-        Conta conta2 = new ContaNegocio(1003, "Bob Silva", 0.0, 200.0);
+        Conta conta2 = new ContaNegocio(1003, "Bob Silva", 0.0, 2000.0);
         Conta conta3 = new ContaPoupanca(1004, "Ana Maria", 0.0, 0.01);
 
         // DOWNCasting
@@ -27,9 +27,11 @@ public class TesteBanco {
             conta5.atualizarSaldoConta();
             System.out.println("Saldo atualizado!");
         }
-         // Print do saldo na conta:
+        // Print do saldo na conta:
          System.out.println("Saldo na conta de Jacqueline Navarro: " + conta.getSaldo());
          System.out.println("Saldo na conta de Jose Silva: " + contaNegocio.getSaldo());
+        // Print do saldo na conta1
+        System.out.println("Saldo na conta1: " + conta1.getSaldo());
 
 
     }
